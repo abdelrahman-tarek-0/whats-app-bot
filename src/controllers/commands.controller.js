3e12ba60a10b138db1e970388536fa5ef9a5ca0d
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-09-27 20:42:24 +0300

    [FORMAT]

--- src/controllers/commands.controller.js
+++ src/controllers/commands.controller.js
@@ -2,17 +2,15 @@ const { v4 } = require('public-ip')
 
 exports.commands = async (message) => {
    const body = message.body
-   console.log('command')
+   console.log('message received')
 
    if (body === '!ping') {
-      console.log('pong')
       message.reply('pong')
    } else if (body === '!help') {
-      console.log('!ping, !help', '!ip')
       message.reply(`!ping, !help, !ip`)
    } else if (body === '!ip') {
       const ip = await v4()
-      console.log(`${ip}`)
+
       message.reply(`${ip}`)
    }
 }
